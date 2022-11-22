95455a14c0fed81934229b0ca3116f6883333ea7
Divya <mdqualityapps@gmail.com>
2022-11-22 19:57:45 +0530

    card created

--- src/App.js
+++ src/App.js
@@ -1,111 +1,111 @@
-import logo from "./logo.svg";
-import "./css/style.css"
-
-import Box from "@mui/material/Box";
-import form, { Button, Stack } from "@mui/material";
+import form, { Button, Paper, Stack } from "@mui/material";
 import { Grid, TextField } from "@mui/material";
 import React from "react";
-import "./css/style.css"
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import "./css/style.css";
+import { maxHeight } from "@mui/system";
 
-// import KeyIcon from '@mui/icons-material/Key';
-import LoginIcon from '@mui/icons-material/Login';
-import { SpeakerNotesOff } from "@mui/icons-material";
-
-const samplepicture =new URL("./images/newedit3.jpg",import.meta.url)
-const untitled1 =new URL("./images/Untitled1.png",import.meta.url)
+const bull = (
+  <Box
+    component="span"
+    sx={{ display: "inline-block", mx: "2px", transform: "scale(0.8)" }}
+  >
+    •
+  </Box>
+);
 
 function App() {
-  return <>
- {/* <body> */}
-       
-         {/* <Box> */}
-          <Grid container justifyContent='center' sx={{ textAlign: 'center' }} >
+  return (
+    <>
+      <Grid container justifyContent="center" sx={{ textAlign: "center" }}>
+        <Box
+          className="sample"
+          sx={{
+            border: "1px solid black",
+            mt: "10%",
+            borderColor: "#d2cbcb;",
+            borderRadius: "40px",
+            ":hover": { boxShadow: 2 },
+            m: 6,
+          }}
+        >
+          <Grid
+            container
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Grid item lg={4} sm={4} xl={4} xs={14} md={4} sx={{ py: 2 }}>
+              <Card sx={{ minWidth: 275, height:150 ,borderRadius:4 }}>
+                <CardContent>
+                  <Typography variant="h5" component="div">
+                   box1
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+            <Grid item lg={4} sm={4} xl={4} xs={14} md={4} sx={{ py: 2 }}>
+              <Card sx={{ minWidth: 275, height:150 ,ml:4,borderRadius:4 }}>
+                <CardContent>
+                  <Typography variant="h5" component="div">
+                   box2
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+            <Grid item lg={4} sm={4} xl={4} xs={14} md={4} sx={{ py: 2 }}>
+              <Card sx={{ minWidth: 275, height:60 ,ml:4,borderRadius:4 }}>
+                <CardContent>
+                  <Typography variant="h5" component="div">
+                   box3
+                  </Typography>
+                </CardContent>
+              </Card>
+              <Card sx={{ minWidth: 275, height:60 ,mt:4,ml:4,borderRadius:4 }}>
+                <CardContent>
+                  <Typography variant="h5" component="div">
+                   box4
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+           
+          </Grid>
+          <Grid
+            container
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Grid item lg={8} sm={4} xl={4} xs={14} md={4} sx={{ py: 2 }}>
+              <Card sx={{ minWidth: 275, height:150 ,borderRadius:4 }}>
+                <CardContent>
+                  <Typography variant="h5" component="div">
+                   box1
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+            <Grid item lg={4} sm={4} xl={4} xs={14} md={4} sx={{ py: 2 }}>
 
-    <Box className="sample"  sx={{ border:"1px solid black",mt:'10%' ,borderColor:'#d2cbcb;' , borderRadius:'40px',':hover': {  boxShadow:2},m:6 }} >
-   <Grid item>
-   
-    <nav className="navbar">
-        <ul>
-
-         <li className="active">Marketplace</li>
-         <li className="active" >Drop</li>
-         <li className="active">Brands</li>
-         <li className="active">Login</li>
-         <li className="active">Sign Up</li>
-        
-
-        </ul>
-      </nav>
+              <Card sx={{ minWidth: 200, height:200 ,mt:4,ml:4,borderRadius:4 }}>
+                <CardContent>
+                  <Typography variant="h5" component="div">
+                   box4
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+           
+          </Grid>
+        </Box>
       </Grid>
-   
-      {/* <Grid justifyContent='center' sx={{ textAlign: 'center' }}> */}
-        <Grid item>
-    <Grid  className="samplepicture">
-     
-    <img  src={samplepicture}/>
-
-    <img className="untitled"  src={ untitled1 }/>
-   
- </Grid>
- {/* <Grid className="box2">
-   
-    </Grid> */}
-    </Grid>
-   
-  <Grid item sx={{pl:5}}>
-     <Box  sx={{ border:"1px solid black",p:3,mt:'30%',borderColor:'#d2cbcb;',width:'210px',backgroundColor:'#C8C8C8', borderRadius:'20px',textAlign:"left"}} >
-  <Box>
-    <h2>Welcome Back</h2>
-    <p>Please Enter Your Email and Password</p>
-    <Grid>
-    <TextField id="Email" label="Email id" variant="outlined" size='small'color='secondary'
-      />
-      </Grid>
-      <Grid sx={{mt:2}}>
-       <TextField    id="MobileNum" label="Password" required variant="outlined"  size='small'color='secondary'
-                                    inputProps={{
-                                        maxLength: 10
-                                    }}
-                               
-                                />
-                                </Grid>
-                                
-                                <Grid container>
-                                <Grid sx={{pt:2}}>
-                                    <Stack spacing={2} direction="row" >
-                                        <Button 
-                                       
-                                         variant="outlined"
-                                            type='submit' sx={{ color: 'white', backgroundColor: '#FFC901', borderRadius: '5px 18px', ':hover': {  borderColor: '#FFC901', color: '#000000'} }}>Login 
-
-                                            <LoginIcon sx={{ml:1}}></LoginIcon>
-                                            </Button>
-                                      
-                                    </Stack>
-                                    
-                                    <Box><p>Dont have on account yet? Create Account</p></Box>
-                              
-                                </Grid>
-                              
-                                </Grid>
-                               
-  </Box>
- <Grid>
- 
- </Grid>
-
- </Box>
- {/* </Grid> */}
-    </Grid>
-  
-    </Box>
-          </Grid> 
-        {/* </Box> */}
-        
-        {/* </body> */}
-      
-        </>
+    </>
+  );
 }
 
 export default App;
-   
